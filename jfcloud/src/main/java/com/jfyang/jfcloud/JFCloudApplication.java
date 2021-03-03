cc71fbdbd7b903f481867f3b646d166b80e4518d
yangjianfeng <yangjianfeng@xiezuoinfo.com>
2021-03-03 19:05:58 +0800

    init project

---
+++ jfcloud/src/main/java/com/jfyang/jfcloud/JFCloudApplication.java
@@ -0,0 +1,24 @@
+package com.jfyang.jfcloud;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan("com.jfyang.jfcloud")
+@EnableScheduling
+public class JFCloudApplication {
+
+	static Logger logger = LoggerFactory.getLogger(JFCloudApplication.class.getName());
+
+	public static void main(String[] args) {
+    	SpringApplication.run(JFCloudApplication.class, args);
+    }
+
+	public void destroy() throws Exception {
+		logger.info("application destroy");
+	}
+}
